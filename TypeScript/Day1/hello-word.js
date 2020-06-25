ac6cacc5d21376c02b62b733def8afe8e7adbcf8
tt <thanh-tai.truong@ekino.com>
2020-06-25 19:54:48 +0700

    first commit

---
+++ TypeScript/Day1/hello-word.js
@@ -0,0 +1,8 @@
+var Person = /** @class */ (function () {
+    function Person() {
+        this.message = "hello word 3";
+    }
+    return Person;
+}());
+var p1 = new Person();
+console.log(p1.message);
---
+++ TypeScript/Day1/hello-word.ts
@@ -0,0 +1,6 @@
+class Person {
+  message: string = "hello word 3"
+}
+
+const p1 = new Person()
+console.log(p1.message)
